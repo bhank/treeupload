43afea4f37652f44d2019630a0e0fc32b1f89c11
Adam Coyne <github@mail2.coyne.nu>
2021-07-03 07:47:48 -0500

    Handle latest smugmug filename weirdness

--- Program.cs
+++ Program.cs
@@ -259,6 +259,11 @@ namespace coynesolutions.treeupload
                     return true;
                 }
             }
+            var mangledName = $"{Path.GetFileNameWithoutExtension(file)}_{Path.GetExtension(file).TrimStart('.')}{Path.GetExtension(file)}";
+            if (albumImages.TryGetValue(Path.GetFileName(mangledName), out matchingImage))
+            {
+                return true;
+            }
             return false;
         }
 
